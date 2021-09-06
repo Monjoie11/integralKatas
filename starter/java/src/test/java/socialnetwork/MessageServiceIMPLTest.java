e9b246e191a9d85d1a4bf878fc1e05b2ec65438a
Jeff Franken <50278140+Monjoie11@users.noreply.github.com>
2021-09-06 17:42:46 -0600

    show time: 1st failure

---
+++ starter/java/src/test/java/socialnetwork/MessageServiceIMPLTest.java
@@ -0,0 +1,14 @@
+package socialnetwork;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class MessageServiceIMPLTest {
+
+  @Test
+  public void testThatAliceExists(){
+    Assertions.assertNotNull(null);
+  }
+
+}
